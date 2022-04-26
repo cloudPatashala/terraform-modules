c5616daeaab6785e2efb3cca50fce9111d013a3e
Cloud <cloud.patashala@gmail.com>
2022-04-26 08:25:11 +0530

    Subnets

---
+++ subnets.tf
@@ -0,0 +1,25 @@
+resource "aws_subnet" "private_subnet" {
+  count = length(var.m_private_subnet_cidr_blocks)
+
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.m_private_subnet_cidr_blocks[count.index]
+  availability_zone = var.m_availability_zones[count.index]
+
+  tags = {
+    Name = "cp-${var.m_batch}-pvt-subnet"
+  }
+}
+
+
+resource "aws_subnet" "public_subnet" {
+  count = length(var.m_public_subnet_cidr_blocks)
+
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.m_public_subnet_cidr_blocks[count.index]
+  availability_zone       = var.m_availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "cp-${var.m_batch}-pub-subnet"
+  }
+}
--- vpc/variable.tf
+++ vpc/variable.tf
@@ -3,3 +3,22 @@ variable "vpc_cidr_block" {
 }
 
 variable "m_batch" {}
+
+variable "m_private_subnet_cidr_blocks" {
+  type        = list(any)
+  default     = [""]
+  description = "list of CIDR for Private subnets."
+}
+
+variable "m_public_subnet_cidr_blocks" {
+  type        = list(any)
+  default     = ["10.0.2.0/24", "10.0.4.0/24"]
+  description = "list of CIDR for Public subnets."
+}
+
+variable "m_availability_zones" {
+  default     = ["ap-south-1a", "ap-south-1b"]
+  type        = list(any)
+  description = "List of availability zones"
+}
+
