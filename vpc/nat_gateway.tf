756f17b2dba349f2a6bef656d09d64abf092f737
Cloud <cloud.patashala@gmail.com>
2022-04-27 00:46:46 +0530

    set count to 0

--- vpc/nat_gateway.tf
+++ vpc/nat_gateway.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "nat_gateway" {
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat_gateway.id
-  subnet_id     = aws_subnet.public_subnet[count.index].id
+  subnet_id     = aws_subnet.public_subnet[0].id
   tags = {
     "Name" = "cp-${var.m_batch}-nat-gateway"
   }
